645399c9350f6e21e0dcbfa61cf38e86cf61c4a2
Fiori <Fiori@FIORI-HOMEPC>
2017-10-15 22:35:20 +0300

    start

--- graphX-01/Program.cs
+++ graphX-01/Program.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace graphX_01
+namespace WindowsFormsProject
 {
     static class Program
     {
         /// <summary>
-        /// Главная точка входа для приложения.
+        /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
